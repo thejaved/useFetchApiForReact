b638ba5df9c9cbd94153752b433de143442c79df
Javed Khan <85006831+thejaved@users.noreply.github.com>
2022-01-05 22:53:53 +0530

    useFetch For React

---
+++ useFetch.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data for that resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setError(error.message);
+      });
+  }, [url]);
+  return { data, isPending, error };
+};
+export default useFetch;
